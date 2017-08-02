d0f72cf31e3cec72c245843f9738d474225d23b1
giMini <pabraeken@gmail.com>
2017-08-02 00:38:33 -0400

    Database connection consitstency

--- action.php
+++ action.php
@@ -1,13 +1,5 @@
 <?php
-$serverName = "SQL01\SQLEXPRESS"; //serverName\instanceName
-$connectionInfo = array("Database"=>"NOAH","UID" => "Administrator","PWD" => "P@ssword3!",);
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-if( $conn ) {    
-}else{
-     echo "La connexion n'a pu être établie.<br />";
-     die(); // print_r( sqlsrv_errors(), true));
-}
+include_once("connection.php");
 include_once("function.php");
 $huntGUID='';
 $serverID='';
--- header.php
+++ header.php
@@ -221,15 +221,7 @@ em{ background-color: yellow }
     <section class="content-header">
       
 <?php
-$serverName = "SQL01\SQLEXPRESS"; //serverName\instanceName
-$connectionInfo = array("Database"=>"NOAH","UID" => "Administrator","PWD" => "P@ssword3!",);
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-if( $conn ) {    
-}else{
-     echo "La connexion n'a pu être établie.<br />";
-     die(); // print_r( sqlsrv_errors(), true));
-}
+include_once("connection.php");
 include_once("function.php");
 $huntGUID='';
 $serverID='';
